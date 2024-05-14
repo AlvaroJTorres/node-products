b2b202efc528fa6800233f5102e809215f38ef46
AlvaroJTorres <mangetrone@gmail.com>
2024-05-14 18:37:05 -0500

    Add token return on register action

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -19,14 +19,23 @@ const registerUser = async (req, res) => {
             return res.status(400).json({message:`Username ${username} is already associated with an account`});
         }
 
-        await db.User.create({
+        const new_user = await db.User.create({
             name,
             username,
             email,
             password: await bcrypt.hash(password, 15),
         });
 
-        return res.status(200).json({message:'Registration successful'});
+        const token = jwt.sign({ id: new_user.id }, secret, {
+          expiresIn: expiration
+      });
+
+      res.status(200).json({data: {
+        id: user.id,
+        name: user.name,
+        username: user.username,
+        accessToken: token,
+    }});
     } catch (err) {
         return res.status(500).json({message:'Error in registering user'});
     }
